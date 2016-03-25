c5b39d6803ec083192a93e0037eb54d13b0d53f0
Norman B. Lancaster <qbradq@gmail.com>
2016-03-25 12:20:34 -0500

    Added support for event with type and property, fires and listens

---
+++ test/fixtures/events.js
@@ -0,0 +1,38 @@
+/**
+ * A function in an evented system.
+ * 
+ * @fires someEvent
+ * @fires someOtherEvent
+ * @listens someGlobalEvent
+ * @listens SomeClass#event:myEvent
+ * @throws Error
+ */
+function foo() {}
+
+/**
+ * Some random event.
+ * 
+ * @event someEvent
+ * @type {String}
+ */
+
+/**
+ * Some other random event.
+ * 
+ * @event someOtherEvent
+ * @type {Object}
+ * @property {String} type The type of event
+ * @property {boolean} important Is this event important?
+ */
+
+/**
+ * A global event with no defined type.
+ * 
+ * @event someGlobalEvent
+ */
+
+/**
+ * A class-specific event
+ * 
+ * @event SomeClass#event:MyEvent
+ */
